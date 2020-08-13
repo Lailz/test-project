eff1d4d8b4609dc7a1244a4100be2d85af389951
Lailz <lailaabdulraheem@gmail.com>
2020-08-13 12:10:54 +0300

    fix port

--- app.js
+++ app.js
@@ -20,15 +20,17 @@ app.use((req, res, next) => {
   res.status(404);
   res.json({
     error: {
-      message: err.message
-    }
+      message: err.message,
+    },
   });
 });
+
+const PORT = process.env.PORT || 8000;
 
 const run = async () => {
   await db.sequelize.sync();
   console.log("Connection to the database successful!");
-  await app.listen(8000, () => console.log(`App is listening on port 8000!`));
+  await app.listen(PORT, () => console.log(`App is listening on port 8000!`));
 };
 
 run();
